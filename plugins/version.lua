3d944b2d1a634b71e2b7c818efb1f08939381721
mehrpouya81 <mehrpouya81@gmail.com>
2015-12-25 13:17:27 -0800

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return 'Telegamer '.. VERSION .. [[ 
+  default source by seed
+  Admins:
+  @iranspm_admin
+  @mt246
+end
+
+return {
+    patterns = {
+    "^[!/]([Vv]ersion)$"
+  }, 
+  run = run 
+}
+
+end
